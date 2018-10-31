98a93ed867e1c1ebe1095c7906ee7a3154696bed
surlena <zemlenur007americashmathphy>
2018-10-31 02:11:40 +0200

    наконец сделала долбаную кнопку

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-	$(".owl-carousel").owlCarousel();
-  });
+	$(".slider").owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 3000
+	});
+
+	$(".btn-nav").on("click", function() {
+		var target = $(this).data("target");
+		$(target).toggleClass("nav__list--open");
+	});
+});
